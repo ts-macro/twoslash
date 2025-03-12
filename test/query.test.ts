ca8d5a63e9d637e3bd46a355d8e51fa9a357bcd0
zhiyuanzmj <260480378@qq.com>
2025-03-12 19:09:52 +0800

    chore: init

---
+++ test/query.test.ts
@@ -0,0 +1,83 @@
+import { describe, expect, it } from 'vitest'
+import { createTwoslasher } from '../src/index'
+
+const code = await import('./fixtures/query-basic.vue?raw').then(m => m.default)
+
+const twoslasher = createTwoslasher()
+
+describe('basic', () => {
+  const result = twoslasher(code, 'vue')
+
+  it('has correct hover types', () => {
+    expect(result.nodes.find(n => n.type === 'hover' && n.target === 'button'))
+      .toHaveProperty('text', '(property) button: ButtonHTMLAttributes & ReservedProps')
+    expect(result.nodes.find(n => n.type === 'hover' && n.target === 'click'))
+      .toHaveProperty('text', `(property) onClick?: ((payload: MouseEvent) => void) | undefined`)
+  })
+
+  it('has correct query', () => {
+    expect(result.meta.positionQueries)
+      .toMatchInlineSnapshot(`
+        [
+          93,
+          161,
+          863,
+          1034,
+        ]
+      `)
+
+    expect(result.nodes.find(n => n.type === 'query' && n.target === 'double'))
+      .toHaveProperty('text', 'const double: ComputedRef<number>')
+
+    expect(result.nodes.find(n => n.type === 'query' && n.target === 'computed'))
+      .toMatchInlineSnapshot(`
+        {
+          "character": 14,
+          "docs": undefined,
+          "length": 8,
+          "line": 1,
+          "start": 39,
+          "tags": undefined,
+          "target": "computed",
+          "text": "(alias) const computed: {
+            <T>(getter: ComputedGetter<T>, debugOptions?: DebuggerOptions): ComputedRef<T>;
+            <T, S = T>(options: WritableComputedOptions<T, S>, debugOptions?: DebuggerOptions): WritableComputedRef<T, S>;
+        }
+        import computed",
+          "type": "query",
+        }
+      `)
+
+    expect(result.nodes.find(n => n.type === 'query' && n.target === 'count'))
+      .toMatchInlineSnapshot(`
+        {
+          "character": 18,
+          "docs": undefined,
+          "length": 5,
+          "line": 9,
+          "start": 228,
+          "tags": undefined,
+          "target": "count",
+          "text": "(property) count: number",
+          "type": "query",
+        }
+      `)
+
+    expect(result.nodes.find(n => n.type === 'query' && n.target === 'click'))
+      .toMatchInlineSnapshot(`
+        {
+          "character": 11,
+          "docs": undefined,
+          "length": 5,
+          "line": 8,
+          "start": 163,
+          "tags": undefined,
+          "target": "click",
+          "text": "(property) onClick?: ((payload: MouseEvent) => void) | undefined",
+          "type": "query",
+        }
+      `)
+    expect(result.nodes.filter(n => n.type === 'query'))
+      .toHaveLength(4)
+  })
+})
---
+++ test/shiki.test.ts
@@ -0,0 +1,67 @@
+import { createTransformerFactory, rendererRich } from '@shikijs/twoslash/core'
+import { createHighlighter } from 'shiki'
+import { expect, it } from 'vitest'
+import { createTwoslasher } from '../src'
+
+const code = await import('./fixtures/example.vue?raw').then(m => m.default)
+
+const styleHeader = [
+  '<head>',
+  `<link rel="stylesheet" href="https://esm.sh/@shikijs/twoslash@1.0.0-beta.5/style-rich.css" />`,
+  `<style>:root { color-scheme: dark; --twoslash-popup-bg: #222; }</style>`,
+  '</head>',
+  '',
+].join('\n')
+
+const twoslasherVue = createTwoslasher()
+const shiki = await createHighlighter({
+  themes: [
+    'vitesse-dark',
+  ],
+  langs: [
+    'vue',
+    'ts',
+    'tsx',
+    'vue',
+  ],
+})
+
+it('highlight vue', async () => {
+  const result = await shiki.codeToHtml(code, {
+    lang: 'vue',
+    theme: 'vitesse-dark',
+    transformers: [
+      createTransformerFactory(twoslasherVue)({
+        langs: ['ts', 'tsx', 'vue'],
+        renderer: rendererRich({
+          lang: 'ts',
+        }),
+      }),
+    ],
+  })
+
+  expect(styleHeader + result)
+    .toMatchFileSnapshot('./results/renderer/example.vue.html')
+})
+
+const twoslasherRaw = createTwoslasher({
+  debugShowGeneratedCode: true,
+})
+
+it('highlight raw', async () => {
+  const result = await shiki.codeToHtml(code, {
+    lang: 'vue',
+    theme: 'vitesse-dark',
+    transformers: [
+      createTransformerFactory(twoslasherRaw)({
+        langs: ['ts', 'tsx', 'vue'],
+        renderer: rendererRich({
+          lang: 'ts',
+        }),
+      }),
+    ],
+  })
+
+  expect(styleHeader + result)
+    .toMatchFileSnapshot('./results/renderer/example.raw.html')
+})
